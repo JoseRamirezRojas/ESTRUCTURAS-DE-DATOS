4b8bb14b16a77f436d9e68859855d38607d087ee
JoseRamirezRojas <josedavidrr@ciencias.unam.mx>
2019-03-25 21:03:12 -0600

    bfs y dfs

--- src/practica5/ArbolBinario.java
+++ src/practica5/ArbolBinario.java
@@ -55,15 +55,63 @@ public abstract class ArbolBinario<T>{
     public abstract boolean contiene(T elemento);
 
     public void bfs(Consumer<T> funcion){
-        funcion.accept(raiz.elemento);
-        T t -> 
+        if(this.raiz!=null){
+            Cola<Vertice> cola=new Cola<>();
+            cola.mete(this.raiz);
+            while (cola.esVacia()==false){
+                Vertice v=cola.mira();
+                if (v.izquierdo!=null)
+                    cola.mete(v.izquierdo);
+                if (v.derecho!=null)
+                    cola.mete(v.derecho);
+                funcion.accept(v.elemento);
+                cola.saca();
+            }
+        }
     }
 
     public void dfs(int tipo, Consumer<T> funcion){
-        switch (tipo){
-
+        switch (tipo) {
+            case 1:
+                //pre order
+                dfsPreOrder(raiz,funcion);
+            case 2:
+                //in order
+                dfsInOrder(raiz,funcion);
+            case 3:
+                //post order
+                dfsPostOrder(raiz,funcion);
         }
     }
+
+    private void dfsPreOrder(Vertice v,Consumer<T> funcion){
+        if(v!=null){
+            funcion.accept(v.elemento);
+            dfsPreOrder(v.izquierdo, funcion);
+            dfsPreOrder(v.derecho,funcion);
+        }
+        return;
+    }
+
+    private void dfsInOrder(Vertice v,Consumer<T> funcion){
+        if(v!=null){
+            dfsInOrder(v.izquierdo,funcion);
+            funcion.accept(v.elemento);
+            dfsInOrder(v.derecho,funcion);
+        }
+        return;
+    }
+
+    private void dfsPostOrder(Vertice v,Consumer<T> funcion){
+        if(v!=null){
+            dfsPostOrder(v.izquierdo,funcion);
+            dfsPostOrder(v.derecho,funcion);
+            funcion.accept(v.elemento);
+
+        }
+        return;
+    }
+
 
     public int getElementos(){
         return elementos;
