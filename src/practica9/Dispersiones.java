51bf28a7368a30d4ca7948c9a24924ee6db27f45
JoseRamirezRojas <josedavidrr@ciencias.unam.mx>
2019-05-19 13:18:07 -0500

    Hashmap 1.0

--- src/practica9/Dispersiones.java
+++ src/practica9/Dispersiones.java
@@ -1,17 +1,40 @@
+package practica9;
+
 /**
  * Clase con varias funciones de dispersión.
  */
 public class Dispersiones{
 
+    /**
+     * Función de dispersión de XOR
+     * @param a Arreglo de bytes
+     * @return  Dispersión de la entrada.
+     */
     public static int dispersionXOR(byte[] a){
 	// Aquí va su código.
+        return 0;
     }
-    
+
+    /**
+     * Función de dispersión de Bob Jenkins
+     * @param a Arreglo de bytes
+     * @return  Dispersión de la entrada.
+     */
     public static int dispersionBJ(byte[] a){
 	// Aquí va su código.
+        return 0;
     }
-    
+
+    /**
+     * Función de dispersión de Daniel J. Bernstein
+     * @param a Arreglo de bytes
+     * @return  Dispersión de la entrada.
+     */
     public static int dispersionDJB(byte[] a){
-	// Aquí va su código.
+        int djb=5381;
+        for (byte b : a) {
+            djb = djb * 33 + (b & 0xFF);
+        }
+        return djb;
     }
 }
--- src/practica9/TablaDeDispersion.java
+++ src/practica9/TablaDeDispersion.java
@@ -1,15 +1,30 @@
+package practica9;
+
+
 /**
  * Clase que define tablas de dispersión.
+ *
+ * Ramírez Rojas José Dvid
+ * Cruz Carmona Uriel
  */
 public class TablaDeDispersion<K, V>{
 
-        private class Entrada {
+    /**
+     * Clase privada de las tablas de dispersión, contiene una llave y un valor.
+     */
+    private class Entrada {
 
     	public K llave;
     	public V valor;
 
+        /**
+         * Constructor de la clase privada Entrada
+         * @param llave Llave K con la que construimos la entrada
+         * @param valor Valor V con la que construimos la entrada
+         */
     	public Entrada(K llave, V valor){
-    	    // Aquí va su código.
+    	    this.llave=llave;
+    	    this.valor=valor;
     	}
     }
  
@@ -19,29 +34,47 @@ public class TablaDeDispersion<K, V>{
     public static final int CAPACIDAD_MINIMA = 64;
     private static final double CARGA_MAXIMA = 0.75;
 
-    
+    /**
+     * Crea un arreglo genérico
+     * @param tamano
+     * @return
+     */
     private Lista<Entrada>[] nuevoArreglo(int tamano){
 	@SuppressWarnings("unchecked")
-	Lista<Entrada>[] arreglo = (Lista<Entrada>[]) new Lista[tamano];
-	return arreglo;
+        Lista<Entrada>[] arreglo = (Lista<Entrada>[]) new Lista[tamano];
+        return arreglo;
     }
 
+    /**
+     * Constructor vacío de una tabla de dispersión
+     */
      public TablaDeDispersion(){
-	 // Aquí va su código.
-        this(CAPACIDAD_MINIMA, (k llave) -> llave.hashCode());
-    }
-    
+
+        this(CAPACIDAD_MINIMA, (K llave )-> llave.hashCode());
+     }
+
+    /**
+     * Constructor de una tabla de dispersión que recibe la capacidad de la misma.
+     * @param capacidad Capacidad de la tabla a crear
+     */
     public TablaDeDispersion(int capacidad){
-	// Aquí va su código.
-        this(capacidad, (k llave) -> llave.hashCode())
+
+        this(capacidad, (K llave) -> llave.hashCode());
     }
 
+    /**
+     * Constructor de una tabla de dispersión que recibe un dispersor.
+     * @param dispersor Dispersor de la tabla a crear
+     */
     public TablaDeDispersion(Dispersor<K> dispersor){
-	// Aquí va su código.
         this(CAPACIDAD_MINIMA,dispersor);
-
     }
 
+    /**
+     * Constructor de una tabla de dispersión que recibe su dispersor y su capacidad
+     * @param capacidad Capacidad de la tabla a crear
+     * @param dispersor Dispersor de la tabla a crear
+     */
     public TablaDeDispersion(int capacidad, Dispersor<K> dispersor){
 	// Aquí va su código.
         this.dispersor = dispersor;
@@ -53,16 +86,20 @@ public class TablaDeDispersion<K, V>{
         tabla = nuevoArreglo(n);
     }
 
+    /**
+     * Recibe una llave y un valor, y agrega el valor a la posición correspondiente a la llave.
+     * @param llave Llave recibida del valor.
+     * @param valor Valor a agregar en la tabla.
+     */
     public void agrega(K llave, V valor){
-	// Aquí va su código.
         if(llave == null || valor == null)
             return;
 
         int dispersion = dispersor.dispersa(llave) & (tabla.length - 1);
 
         if (tabla[dispersion] == null) {
-            entrada[dispersion] = new Lista<Entrada>();
-            entrada[dispersion].agrega(new (llave,valor));
+            tabla[dispersion] = new Lista<>();
+            tabla[dispersion].agregaFinal(new Entrada(llave,valor));
             elementos++;
         } else {
             boolean estaLlave = false;
@@ -76,55 +113,175 @@ public class TablaDeDispersion<K, V>{
 
             if (!estaLlave) {
                 elementos++;
-                tabla[dispersion].agrega(new Entrada(llave,valor));
+                tabla[dispersion].agregaFinal(new Entrada(llave,valor));
             }
         }
+
+        if (getCarga() >= CARGA_MAXIMA)
+            incrementaTabla();
     }
- 
+
+    /**
+     * Recibe una llave y regresa el valor asociado a ella, si existe.
+     * @param llave LLave de la que se quiere obtener su valor asociado.
+     * @return      Valor de la llave recibida.
+     */
     public V getValor(K llave){
-	// Aquí va su código.
+        if (llave == null)
+            return null;
+
+        Lista<Entrada> listaTabla = tabla[dispersor.dispersa(llave) & (tabla.length-1)];
+        V valor = null;
+        for (Entrada entrada : listaTabla)
+            if (entrada.llave.equals(llave))
+                valor = entrada.valor;
+
+        return valor;
     }
 
+    /**
+     * Indica si la llave recibida está contenida en la tabla.
+     * @param llave     Llave que queremos saber si está en la tabla.
+     * @return          <tt>true</tt>   Si la llave está en la tabla.
+     *                  <tt>false</tt>  En otro caso.
+     */
     public boolean contieneLlave(K llave){
-	// Aquí va su código.
+        if (llave == null)
+            return false;
+
+        Lista<Entrada> listaTabla = tabla [dispersor.dispersa(llave) & (tabla.length-1)];
+        for (Entrada entrada : listaTabla)
+            if (entrada.llave.equals(llave))
+                return true;
+
+        return false;
     }
 
+    /**
+     * Indica si el valor recibido está contenido en la tabla.
+     * @param valor     Valor que queremos saber si está en la tabla.
+     * @return          <tt>true</tt>   Si el valor está en la tabla.
+     *                  <tt>false</tt>  En otro caso.
+     */
     public boolean contieneValor(V valor){
-	// Aquí va su código.
+        if (valor == null)
+            return false;
+
+//        Lista<Entrada> listaEntradas = tabla[dispersor.dispersa() & (tabla.length-1)];
+//        for (Entrada entrada : listaEntradas)
+//            if (entrada.valor.equals(valor))
+//                return true;
+
+
+        //for(Entrada entrada: tabla)
+
+        return false;
     }
 
+    /**
+     * Indica si la tabla es vacía.
+     * @return       <tt>true</tt>   Si la tabla está vacía.
+     *               <tt>false</tt>  En otro caso.
+     */
     public boolean esVacia(){
-	// Aquí va su código.
+	    return elementos==0;
     }
 
+    /**
+     * Elimina el elemento asociado a la llave.
+     * @param llave  Llave del elemento a eliminar
+     * @return
+     */
     public V elimina(K llave){
-	// Aquí va su código.
+        if (llave == null)
+            return null;
+
+        int llaveDispersada = dispersor.dispersa(llave) & (tabla.length-1);
+        Lista<Entrada> listaTabla = tabla[dispersor.dispersa(llave) & (tabla.length-1)];
+
+        if(listaTabla == null)
+            return null;
+
+        for (Entrada entrada : listaTabla) {
+            if (entrada.llave.equals(llave)) {
+                listaTabla.elimina(entrada);
+                elementos--;
+                if (listaTabla.getLongitud()==0)
+                    tabla[llaveDispersada] = null;
+                return entrada.valor;
+            }
+        }
+        
+        return null;
     }
 
+    /**
+     * Regresa el número de elementos de la tabla.
+     * @return  Variable de clase elementos
+     */
     public int getElementos(){
-	// Aquí va su código.
+	    return elementos;
     }
 
+    /**
+     * Regresa una lista con todas las llaves de la tabla.
+     * @return  Lista con las llaves de la tabla.
+     */
     public Lista<K> getLlaves(){
-	// Aquí va su código.
-        Lista<K> llaves = new Lista<K>();
+        Lista<K> llaves = new Lista<>();
 
         for (Lista<Entrada> lista : tabla)
             if (lista != null)
                 for (Entrada entrada : lista)
-                    llaves.agrega(entrada.llave);
+                    llaves.agregaFinal(entrada.llave);
+
         return llaves;
     }
 
+    /**
+     * Regresa una lista con todos los valores de la tabla.
+     * @return  Lista con los valores de la tabla.
+     */
     public Lista<V> getValores(){
-	// Aquí va su código.
-        Lista<V> valores = new Lista<V>();
+        Lista<V> valores = new Lista<>();
 
         for (Lista<Entrada> lista : tabla)
             if (lista != null)
                 for (Entrada entrada : lista)
-                    valores.agrega(entrada.llave);
-        
+                    valores.agregaFinal(entrada.valor);
+
         return valores;
-    } 
+    }
+
+    /**
+     * Regresa la carga actual del diccionario.
+     * @return Carga del diccionario.
+     */
+    public double getCarga() {
+        return (double)elementos/tabla.length;
+    }
+
+    /**
+     * Incrementa el tamaño de la tabla al doble.
+     */
+    private void incrementaTabla() {
+        Lista<Entrada>[] diccionario = tabla;
+        tabla = nuevoArreglo(diccionario.length*2);
+        int llaveDispersada;
+
+        for (int i = 0; i < diccionario.length; i++) {
+            if (diccionario[i] != null) {
+                for (Entrada entrada : diccionario[i]) {
+                    llaveDispersada = dispersor.dispersa(entrada.llave) & (tabla.length-1);
+                    if (tabla[llaveDispersada] != null) {
+                        tabla[llaveDispersada].agregaFinal(entrada);
+                    } else {
+                        Lista<Entrada> nuevaEntrada = new Lista<Entrada>();
+                        nuevaEntrada.agregaFinal(entrada);
+                        tabla[llaveDispersada] = nuevaEntrada;
+                    }
+                }
+            }
+        }
+    }
 }
