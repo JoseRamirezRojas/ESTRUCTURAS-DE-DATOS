3ee4d08542193e6fc0bc7664869d671f67ac90a1
Uriel-Cruz <uriel_26@ciencias.unam.mx>
2019-05-30 18:12:58 -0500

    Proba

--- src/Proyecto3/src/proyecto3/Probabilidad.java
+++ src/Proyecto3/src/proyecto3/Probabilidad.java
@@ -8,23 +8,55 @@ import java.io.Serializable;
  * @author Cruz Carmona Uriel
  */
 public class Probabilidad implements Serializable{
+
+    public Probabilidad() {
+    }
+    
     
     public Equipos determinaGanador(Equipos e1, Equipos e2){
+        double prob1 = e1.getHabilidad() / e1.getHabilidad() + e2.getHabilidad();
+        
+        
+        double rand = Math.random();
+        
+        if(rand >= 0 && rand <= prob1){
+            return e1;
+        }
+        return e2;
+        
+        
         
         
     }
     
-    public Equipos cuotaDecimal(Equipos e1){
+    public double cuotaDecimal1(Equipos e1, Equipos e2){
+        double prob1 = e1.getHabilidad() / e1.getHabilidad() + e2.getHabilidad();
+       
+        double cuota1;
+        
+        cuota1 = 1/ prob1;
+        
+        return cuota1;
+        
+        
+        
+        
+    }
+    public double cuotaDecimal2(Equipos e1, Equipos e2){
+       
+        double prob2 = e2.getHabilidad() / e2.getHabilidad() + e1.getHabilidad();
+        double cuota2;
+        
+        cuota2 = 1/ prob2;
+        
+        return cuota2;
+        
+        
         
         
     }
     
-    public Equipos calculaApuesta(float apuesta){
-        
-        
-    }
-    
-    
+ 
     
       
 }
